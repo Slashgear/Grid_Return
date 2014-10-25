a7181ef27a72a39e4a71e67261da663ed0730bdc
xenyztw <chaussende.adrien@gmail.com>
2014-10-25 19:27:46 +0200

    [ACE]: Ajout de la classe Direction

---
+++ Assets/Scripts/Direction.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Direction {
+	public static Vector2 UP = new Vector2(0,1);
+	public static Vector2 DOWN = new Vector2(0,-1);
+	public static Vector2 LEFT = new Vector2(-1,0);
+	public static Vector2 RIGHT = new Vector2(1,0);
+}
